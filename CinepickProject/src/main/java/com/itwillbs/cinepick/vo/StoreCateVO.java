b761f765dc65a8ad885dd073d19af874df797efe
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-11-06 18:48:38 +0900

    [김주영 스토어/카테고리 VO]

---
+++ CinepickProject/src/main/java/com/itwillbs/cinepick/vo/StoreCateVO.java
@@ -0,0 +1,9 @@
+package com.itwillbs.cinepick.vo;
+
+import lombok.Data;
+
+@Data
+public class StoreCateVO {
+	int storeCate_Idx;
+	String storeCate_Subject;
+}
---
+++ CinepickProject/src/main/java/com/itwillbs/cinepick/vo/StoreVO.java
@@ -0,0 +1,21 @@
+package com.itwillbs.cinepick.vo;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
+public class StoreVO {
+	private int store_idx;
+	private String store_short;
+	private String store_title;
+	private String store_price;
+	private String store_thumbnail;
+	private int store_category;
+	
+//	private MultipartFile event_poster_multi;
+//	private MultipartFile event_thumbnail_multi;
+//	private String event_status;
+//
+//	private String eventCate_Subject;
+}
